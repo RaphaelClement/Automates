b056e12d7169233da5abc1b2ca1d64da5ee42944
RaphaelClement <raphael.clement0+1@gmail.com>
2024-01-04 09:56:58 +0100

    update automate.c

--- src/automate.c
+++ src/automate.c
@@ -18,3 +18,38 @@ struct Automate *creer_automate() {
   }
   return automate;
 }
+
+void ajoute_etat(struct Automate *automate, int id) {
+  printf("\tAjout d’un état %d\n", id);
+
+  if (automate != NULL) {
+    // Augmenter la taille du tableau des états
+    automate->etats = realloc(automate->etats, (automate->etat_nb + 1) * sizeof(struct Etat *));
+
+    // Allouer de la mémoire pour le nouvel état
+    automate->etats[automate->etat_nb] = malloc(sizeof(struct Etat));
+   	 
+    if (automate->etats[automate->etat_nb] != NULL) {
+      automate->etats[automate->etat_nb]->id = id;
+      automate->etat_nb++;
+    } else {
+      fprintf(stderr, "Erreur : Impossible d'allouer de la mémoire pour l'état %d\n", id);
+    }
+  } else {
+    fprintf(stderr, "Erreur : L'automate est NULL\n");
+  }
+}
+
+
+void ajoute_transition(struct Automate *automate, int from, int to, char carac) {
+  printf("\tAjout d’un transition de %d à %d avec le caractère %c\n", from, to, carac);
+
+  if (automate != NULL) {
+    // Augmenter la taille du tableau des transitions
+    automate->transitions = realloc(automate->transitions, (automate->transition_nb + 1) * sizeof(struct Transition *));
+
+    // Allouer de la mémoire pour la nouvelle transition
+    automate->transitions[automate->transition_nb] = malloc(sizeof(struct Transition));
+
+    if (automate->transitions[automate->transition_nb] != NULL) {
+      automate->transitions[automate->transition_nb]->from = automate->etats[from];
